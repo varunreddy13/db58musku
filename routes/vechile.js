6f03fb089b4011bfe3d8729695da994a81585a2b
varunreddy13 <varunreddymusku@gmail.com>
2021-11-10 19:22:14 -0600

    Update vechile.js

--- routes/vechile.js
+++ routes/vechile.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('vechile', { title: 'Search Results Vechile' });
-});
-
-module.exports = router;
+var express = require('express'); 
+const vechile_controlers= require('../controllers/vechile'); 
+var router = express.Router(); 
+ 
+/* GET costumes */ 
+router.get('/', vechile_controlers.vechile_view_all_Page ); 
+module.exports = router; 
