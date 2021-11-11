dc19742c53da892ff956abc0aef390a0a8dbad3e
varunreddy13 <varunreddymusku@gmail.com>
2021-11-10 19:25:35 -0600

    Update vechile.js

--- controllers/vechile.js
+++ controllers/vechile.js
@@ -47,3 +47,24 @@ exports.vechile_view_all_Page = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }   
 }; 
+// Handle Costume create on POST. 
+exports.vechile_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new vechile(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    document.vName = req.body.vName; 
+    document.vEngine = req.body.vEngine; 
+    document.vCost = req.body.vCost; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+}; 
+ 
