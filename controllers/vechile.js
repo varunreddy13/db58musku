8f5e9ad0081b574aa61d637ee9a55196ad3342d6
varunreddy13 <varunreddymusku@gmail.com>
2021-11-12 01:37:56 -0600

    changes made

--- controllers/vechile.js
+++ controllers/vechile.js
@@ -79,4 +79,24 @@ exports.vechile_detail = async function(req, res) {
         res.send(`{"error": document for id ${req.params.id} not found`); 
     } 
 }; 
- 
+
+// Handle vechile update form on PUT. 
+exports.vechile_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await vechile.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.vName)  
+               toUpdate.vName = req.body.vName; 
+        if(req.body.vEngine) toUpdate.vEngine = req.body.vEngine; 
+        if(req.body.vCost) toUpdate.vCost = req.body.vCost; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
