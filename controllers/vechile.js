7dc7b7b286593b137a865cb188f9a3334911b304
varunreddy13 <varunreddymusku@gmail.com>
2021-11-12 01:25:31 -0600

    changes made

--- controllers/vechile.js
+++ controllers/vechile.js
@@ -67,4 +67,16 @@ exports.vechile_create_post = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }   
 }; 
+
+// for a specific Costume. 
+exports.costume_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Costume.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
  
