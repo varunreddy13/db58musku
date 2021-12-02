c41ff2b96657dd2649033f2cfd8ef0e588ec077a
varunreddy13 <varunreddymusku@gmail.com>
2021-12-01 18:28:45 -0600

    changes added

--- models/vechile.js
+++ models/vechile.js
@@ -1,8 +1,18 @@
 const mongoose = require("mongoose") 
 const costumeSchema = mongoose.Schema({ 
     vName: String, 
-    vEngine: String, 
-    vCost: Number 
+    vEngine: {
+        type: Number,
+        min: 1,
+        max: 900,
+      
+    }, 
+    vCost:{
+        type: Number,
+        min: 200,
+        max: 500,
+      
+    }
 }) 
  
 module.exports = mongoose.model("vechile", 
